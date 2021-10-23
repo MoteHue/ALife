0dd1242d37ce2078c1704a06a694e0a8d02cdbf1
Matt <matthew.crees@ntlworld.com>
2021-10-23 15:44:16 +0100

    initial project

---
+++ Lab Recreation/Genetic Algorithm/Genetic Algorithm/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Genetic_Algorithm {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
