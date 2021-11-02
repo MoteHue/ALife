f967f9a2f12774e00a85d512402908029a726ef9
Matt <matthew.crees@ntlworld.com>
2021-11-02 12:31:44 +0000

    updated grid generation

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -55,17 +55,21 @@ public class GridManager : MonoBehaviour
         }
     }
 
-    public void ButtonSetupCellGrid() {
+    public void ButtonSetupCellGrid(Toggle t) {
         width = int.Parse(gridManagementInputFields[0].text);
         height = int.Parse(gridManagementInputFields[1].text);
         depth = int.Parse(gridManagementInputFields[2].text);
         SetupCellGrid();
-        if (cellsVisualised) SetupVisualCellGrid();
+        if (t.isOn) {
+            SetupVisualCellGrid();
+            cellsVisualised = true;
+        }
         GenerateGridButton.interactable = false;
-    }
-
-    public void ChangeCellVisualised(bool b) {
-        cellsVisualised = b;
+        GenerateGridButton.GetComponentInChildren<Text>().text = "Grid Generated";
+        foreach (InputField field in gridManagementInputFields) {
+            field.interactable = false;
+        }
+        t.interactable = false;
     }
 
     void ChangeCell(int x, int y, int z, int value, bool visible) {
@@ -73,7 +77,7 @@ public class GridManager : MonoBehaviour
         if (cellsVisualised) {
             CellData cell = visualCells[x][y][z].GetComponent<CellData>();
             cell.ActivateMesh(visible);
-        }   
+        }
     }
 
     public void ButtonChangeCell(bool b) {
@@ -84,6 +88,22 @@ public class GridManager : MonoBehaviour
             if (b) ChangeCell(x, y, z, 1, b);
             else ChangeCell(x, y, z, 0, b);
         }
+    }
+
+    public void DebugLogBigOlList() {
+        string debugString1 = "";
+        for (int x = 0; x < width; x++) {
+            string debugString2 = $"X{x}:\n";
+            for (int y = 0; y < height; y++) {
+                string debugString3 = $"Y{y}: ";
+                for (int z = 0; z < depth; z++) {
+                    debugString3 += $"{cells[x][y][z]}, ";
+                }
+                debugString2 += $"{debugString3}\n";
+            }
+            debugString1 += $"{debugString2}\n";
+        }
+        Debug.Log(debugString1);    
     }
 
 }
